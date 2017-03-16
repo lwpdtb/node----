00045156d02c0e2b2967e0ab015cbf27fb37907f
lwpdtb <2364357112@qq.com>
2017-03-16 17:35:47 +0800

    最简单的服务器

---
+++ http/http.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lwp on 2017/3/16.
+ */
+var http=require('http')
+
+http.createServer(
+    function (req,res) {
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.write('hello!!world')
+        res.end()
+    }
+).listen(2015)
